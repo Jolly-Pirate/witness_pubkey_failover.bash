b2b057fe9848da07c843ef7146ccfb66de7cf844
steempty <steempty@gmx.com>
2016-06-08 08:07:00 +0300

    script and readme

---
+++ README.md
@@ -0,0 +1,22 @@
+## Witness pubkey failover script
+
+as Witness it is recommended to run a hot backup server, synced and running with witness configuration, and using additional pubkey/privkey pair.
+
+if the script identifies that additional missed blocks threshold is reached, it is updating the witness's pubkey to use the defined backup block signing pubkey (defined in the script).
+
+it provides automatic failover, without failback.
+the script exists after updating the public key.
+
+*** the script does not monitor if the witness server, and the backup servers are up.
+
+### Requirements
+* a local (not on the witness Server) steemd, and a cli wallet running with
+  ```
+   $ ./cli_wallet -r127.0.0.1:8090 --rpc-http-allowip 127.0.0.1
+  ```
+* 2 witness servers, one primary and other secondary, using different pubkey/privkey pair.
+
+### Configuration
+
+edit the variables in the script to match your configuration. make sure all the values are correct.
+
---
+++ witness_pubkey_failover.bash
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+account="accoutname"
+init_misses=113  #witness current total_misses count
+misses_failover_count=2   #misses count threshold for failing over to backup pubkey
+backup_pub_signing_key=STMbackuppubkey
+wallet="http://localhost:8090/rpc"  #steemd's rpc
+wallet_passphrase="walletpassphrase"
+props='{"account_creation_fee":"10.000 STEEM","maximum_block_size":131072,"sbd_interest_rate":1000}'
+witness_url="https://steemit.com/witness-category/@accountname/witness-post"  #edit to match your witness URL/CV
+
+##
+
+function check_misses {
+  misses="$(curl --data-ascii '{"id":0,"method":"get_witness","params":["'"$account"'"]}' \
+			-s "$wallet" \
+		     | sed "s/,/\n/g" \
+		     | grep "total_missed" \
+		     | cut -d":" -f 2)"
+  echo $misses
+}
+
+misses=`check_misses`
+
+if [ $misses -ne $init_misses ] ; then
+  echo "failed for fetch misses from steemd, \$init_misses is not equal to \$misses"
+  exit 1
+fi
+
+echo "[`date`] init misses: $init_misses"
+
+while true ; do 
+  misses=`check_misses`
+  if [ -z "`echo $misses | grep -E '[[:digit:]]'`" ] ; then
+    echo "[`date`] failed to fetch misses from steemd (not a number)"
+    sleep 10s
+    continue
+  fi
+  echo "[`date`] misses: $init_misses"
+  if [ $(($misses-$init_misses)) -ge $misses_failover_count ] ; then
+     echo "[`date`] FAILOVER updating witness public signing key"
+     curl -H "content-type: application/json" -X POST -d "{\"id\":0,\"method\":\"unlock\",\"params\":[\"$wallet_passphrase\"]}" $wallet
+     curl -H "content-type: application/json" -X POST -d "{\"id\":0,\"method\":\"update_witness\",\"params\":[\"$account\",\"$witness_url\",\"$backup_pub_signing_key\",$props,true]}" $wallet 2>/dev/null
+     curl -H "content-type: application/json" -X POST -d "{\"id\":0,\"method\":\"lock\",\"params\":[]}" $wallet
+     exit 2
+  fi
+  sleep 10s
+done
